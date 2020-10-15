48c0921ac073947db3fe8cb6a145d815c4efe3da
Anshuman Kumar Sharma <35607867+ans-sharma@users.noreply.github.com>
2020-10-15 23:15:41 +0530

    Working

---
+++ repeatingSequence.java
@@ -0,0 +1,15 @@
+// Java Program to find the longest repeating sequence in a string
+// I/P : str = &quot;acbdfghybdf&quot;
+// O/P : bdf
+import java.util.Scanner;
+public class repeatingSequence(){
+    public static void main(String[] args) {
+        String sText;
+        Scanner input = new Scanner(System.out);
+        System.out.print("Enter the String Sequence: ");
+        sText = input.nextLine();
+        for( int i = 0; i < sText.length(); i++){
+            for
+        }
+    }
+}
