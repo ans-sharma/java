e7fd780f4b2f2d7eafe4b9e3624214aed558fb3b
Anshuman Kumar Sharma <35607867+ans-sharma@users.noreply.github.com>
2020-10-18 10:09:20 +0530

    Structure Completed

--- EmployeeDetails.java
+++ EmployeeDetails.java
@@ -12,3 +12,62 @@
 // ProjectLeader: with the extra members projectId and projectAllowance.
 // Override getGrossSalary to include the commission in case of managers, and show all details for both of
 // the derived class by overriding the showDetails method.
+
+class Employee {
+    private String Name, Address, PAN;
+    private int BasicSalary;
+
+    void showDetails() {
+        System.out.print("Name: " + Name);
+        System.out.print("Address: " + Address);
+        System.out.print("PAN: " + PAN);
+    }
+
+    int getGrossSalary() {
+        return BasicSalary;
+    }
+
+    void setData(String Name, String Address, String PAN, int BasicSalary) {
+        this.Name = Name;
+        this.Address = Address;
+        this.PAN = PAN;
+        this.BasicSalary = BasicSalary;
+    }
+}
+
+class Manager extends Employee {
+    private String Department;
+    private int Commision;
+
+    int getGrossSalary() {
+        return Commision + super.getGrossSalary();
+    }
+
+    void showDetails() {
+        super.showDetails();
+        System.out.print("Department: " + Department);
+    }
+
+    void setManagerData(String Name, String Address, String PAN, int BasicSalary, String Department, int Commision) {
+        super.setData(Name, Address, PAN, BasicSalary);
+        this.Department = Department;
+        this.Commision = Commision;
+    }
+}
+
+class ProjectLeader extends Employee {
+    private String ProjectId;
+    private int ProjectAllowance;
+
+    void showDetails() {
+        super.showDetails();
+        System.out.print("ProjectId: " + ProjectId);
+        System.out.print("ProjectAllowance: " + ProjectAllowance);
+    }
+
+    void setManagerData(String Name, String Address, String PAN, int BasicSalary, String ProjectId, int ProjectAllowance) {
+        super.setData(Name, Address, PAN, BasicSalary);
+        this.ProjectId = ProjectId;
+        this.ProjectAllowance = ProjectAllowance;
+    }
+}
