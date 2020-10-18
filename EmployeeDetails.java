b48d8cf58cc2e2ef6952bfc69d777d7933b96f8c
Anshuman Kumar Sharma <35607867+ans-sharma@users.noreply.github.com>
2020-10-18 08:43:20 +0530

    New

---
+++ EmployeeDetails.java
@@ -0,0 +1,14 @@
+// Create a class employee with the following members:
+// a. Name
+// B. Address
+// c. PAN number
+// d. Base Salary
+// And the following methods:
+// a. ShowDetails()
+// b. getGrossSalary()
+// Along with getters and setters (accessors and mutators) for the member variables.
+// Derive two classes from it:
+// Manager: with the extra members commision and department.
+// ProjectLeader: with the extra members projectId and projectAllowance.
+// Override getGrossSalary to include the commission in case of managers, and show all details for both of
+// the derived class by overriding the showDetails method.
