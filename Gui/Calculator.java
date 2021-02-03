6ac95f88d6e9a9db2fbbf28a566a17751eeecb80
Anshuman Kumar Sharma <35607867+ans-sharma@users.noreply.github.com>
2021-02-03 09:21:27 +0530

    first commit

--- Gui/Calculator.java
+++ Gui/Calculator.java
@@ -1,3 +1,150 @@
-public class Calculator {
-    
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Calculator extends JFrame implements ActionListener{
+    JFrame frame;
+    JButton btnPlus, btnMinus, btnDiv, btnMul, btnMod, btnCls;
+    JTextField txtNo1, txtNo2;
+    JLabel lResult, lNo1, lNo2, lNoResult;
+    Calculator(){
+        //frame
+        frame = new JFrame("Swing Arithmetics");
+        //buttons
+        btnPlus = new JButton("+");
+        btnMinus = new JButton("-");
+        btnDiv = new JButton("/");
+        btnMul = new JButton("*");
+        btnMod = new JButton("%");
+        btnCls = new JButton("CLEAR");
+        //textfields
+        txtNo1 = new JTextField();
+        txtNo2 = new JTextField();
+        //labels
+        lResult = new JLabel("Result");
+        lNo1 = new JLabel("First No.");
+        lNo2 = new JLabel("Second No.");
+        lNoResult = new JLabel("");
+
+        //set labels
+        lNo1.setBounds(20, 20, 90, 20);
+        lNo2.setBounds(20, 45, 90, 20);
+        lResult.setBounds(20, 70, 90, 20);
+        lNoResult.setBounds(100, 70, 160, 20);
+
+        //set textfields
+        txtNo1.setBounds(100, 20, 160, 20);
+        txtNo2.setBounds(100, 45, 160, 20);
+        
+        //set alignment
+        txtNo1.setHorizontalAlignment(4);  //SwingConstants.RIGHT returns (int)4
+        txtNo2.setHorizontalAlignment(4);
+        lNoResult.setHorizontalAlignment(4);
+
+        //set btn
+        btnPlus.setBounds(20, 100, 120, 30);
+        btnMinus.setBounds(140, 100, 120, 30);
+        btnMul.setBounds(20, 132, 120, 30);
+        btnDiv.setBounds(140, 132, 120, 30);
+        btnMod.setBounds(20, 164, 120, 30);
+        btnCls.setBounds(140, 164, 120, 30);
+
+        //add ActionListsener
+        btnPlus.addActionListener(this);
+        btnMinus.addActionListener(this);
+        btnMul.addActionListener(this);
+        btnDiv.addActionListener(this);
+        btnMod.addActionListener(this);
+        btnCls.addActionListener(this);
+
+
+        //frame setting
+        frame.setSize(300,300);
+        frame.setLayout(null);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+        //add button
+        frame.add(btnPlus);
+        frame.add(btnMinus);
+        frame.add(btnDiv);
+        frame.add(btnMul);
+        frame.add(btnMod);
+        frame.add(btnCls);
+        //add textfield
+        frame.add(txtNo1);
+        frame.add(txtNo2);
+        //add labels
+        frame.add(lNo1);
+        frame.add(lNo2);
+        frame.add(lResult);
+        frame.add(lNoResult);
+    }
+
+    public void actionPerformed(ActionEvent e){
+        if(e.getSource()==btnCls){
+            txtNo1.setText("");
+            txtNo2.setText("");
+            lNoResult.setText("");
+        }
+        else{
+            if(e.getSource()==btnPlus){
+                if (numcheck(txtNo1.getText()) && numcheck(txtNo2.getText())){
+                    double temp = Double.parseDouble(txtNo1.getText()) + Double.parseDouble(txtNo2.getText());
+                    lNoResult.setText(Double.toString(temp));
+                }
+                else
+                    lNoResult.setText("Value Error!");
+            }
+            if(e.getSource()==btnMinus){
+                if (numcheck(txtNo1.getText()) && numcheck(txtNo2.getText())){
+                    double temp = Double.parseDouble(txtNo1.getText()) - Double.parseDouble(txtNo2.getText());
+                    lNoResult.setText(Double.toString(temp));
+                }
+                else
+                    lNoResult.setText("Value Error!");
+            }
+            if(e.getSource()==btnDiv){
+                if (numcheck(txtNo1.getText()) && numcheck(txtNo2.getText())){
+                    double temp = Double.parseDouble(txtNo1.getText()) / Double.parseDouble(txtNo2.getText());
+                    lNoResult.setText(Double.toString(temp));
+                }
+                else
+                    lNoResult.setText("Value Error!");
+            }
+            if(e.getSource()==btnMul){
+                if (numcheck(txtNo1.getText()) && numcheck(txtNo2.getText())){
+                    double temp = Double.parseDouble(txtNo1.getText()) * Double.parseDouble(txtNo2.getText());
+                    lNoResult.setText(Double.toString(temp));
+                }
+                else
+                    lNoResult.setText("Value Error!");
+            }
+            if(e.getSource()==btnMod){
+                if (numcheck(txtNo1.getText()) && numcheck(txtNo2.getText())){
+                    double temp = Double.parseDouble(txtNo1.getText()) % Double.parseDouble(txtNo2.getText());
+                    lNoResult.setText(Double.toString(temp));
+                }
+                else
+                    lNoResult.setText("Value Error!");
+            }
+        }
+    }
+    public static boolean numcheck(String strNum) {
+        if (strNum == null) {
+            return false;
+        }
+        try {
+            double d = Double.parseDouble(strNum);
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+        return true;
+    }
+
+    //driver
+    public static void main(String[] args) {
+        Calculator cal = new Calculator();
+    }
+
 }
--- Gui/TempCovert.java
+++ Gui/TempCovert.java
@@ -4,18 +4,72 @@ import javax.swing.*;
 public class TempCovert extends JFrame implements ActionListener{
     JFrame frame;
     JTextField tempCel;
-    JLabel lCel, lFer, lFerResult;
+    JLabel lCel, lFer, lFerResult, lCelDeg;
     JButton btnConvert;
     TempCovert(){
-        frame = new JFrame();
+        frame = new JFrame("TempConvert");
         tempCel = new JTextField();
         lCel = new JLabel("Temperature in C:");
         lFer = new JLabel("Temperature in F:");
         lFerResult = new JLabel("");
+        lCelDeg = new JLabel(" degC");
         btnConvert = new JButton("CONVERT");
+
+        //set label
+        lCel.setBounds(10, 20, 150, 20);
+        lCelDeg.setBounds(200, 20, 50, 20);
+        lFer.setBounds(10, 50, 150, 20);
+        lFerResult.setBounds(150, 50, 80, 20);
+
+        //set textfield
+        tempCel.setBounds(150, 20, 50, 20);
+
+        //set button
+        btnConvert.setBounds(100, 80, 100, 30);
+        // add listener
+        btnConvert.addActionListener(this);
+
+        //main
+        frame.setSize(300,300);
+        frame.setVisible(true);
+        frame.setLayout(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //add label
+        frame.add(lCel);
+        frame.add(lFer);
+        frame.add(tempCel);
+        frame.add(lFerResult);
+        frame.add(lFerResult);
+        frame.add(btnConvert);
+        frame.add(lCelDeg);
     }
     public void actionPerformed(ActionEvent e){
-        ;
+        if(e.getSource()==btnConvert){
+            if(numcheck(tempCel.getText())){
+                double temp = ((9*Double.parseDouble(tempCel.getText()))/5) + 32; //formula
+                lFerResult.setText(Double.toString(temp) + " degF");
+            }
+            else
+                lFerResult.setText("Value Error!");
+        }
+    }
+
+    public static boolean numcheck(String strNum) {
+        if (strNum == null) {
+            return false;
+        }
+        try {
+            double d = Double.parseDouble(strNum);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        TempCovert t = new TempCovert();
     }
 
 }
